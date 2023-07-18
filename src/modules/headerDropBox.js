e89f4ae7cf3f24d65ae03df585c19c38fa104a9e
Maschkov Konstantin <maschkovc@yandex.ru>
2023-07-18 13:47:27 +0300

    dropdown hrader

--- src/modules/headerDropBox.js
+++ src/modules/headerDropBox.js
@@ -2,8 +2,9 @@
 export const dropBox = () => {
     const movies = document.querySelector('.menu__list-item--dropDown'),
     headerDropDownMovie = document.querySelector('.header__dropDown'),
-    headerWrapper = document.querySelector('.header__flex-wrapper');
-
+    headerWrapper = document.querySelector('.header__flex-wrapper'),
+    body = document.querySelector('.body'),
+    dropDownMovie = document.querySelector('.header__dropDownMovie');
 
     movies.addEventListener('mouseover', () => {
         headerDropDownMovie.classList.add('header__dropDown--active');
@@ -11,11 +12,11 @@ export const dropBox = () => {
 
     })
 
-    headerDropDownMovie.addEventListener('mouseout', () => {
+    // хотелось бы конечно чтобы оно исчезало если мышь уводят с блока  а не по клику
+    body.addEventListener('click', () => {
         headerDropDownMovie.classList.remove('header__dropDown--active');
         headerWrapper.classList.remove('header__flex-wrapper--active');
-    })
-    
+    })  
 }
 
 
